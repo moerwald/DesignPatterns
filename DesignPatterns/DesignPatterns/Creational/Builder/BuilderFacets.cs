c6f61d2a7195ee22e2c5a23d0583e3aa25d402f3
andreas mewald <andreas.mewald@pdts.at>
2017-07-11 07:45:52 +0200

    Intial commit

---
+++ DesignPatterns/DesignPatterns/Creational/Builder/BuilderFacets.cs
@@ -0,0 +1,127 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+
+/// <summary>
+/// This file contains an example of a fluent person builder facade.
+/// </summary>
+namespace DesignPatterns.Creational.Builder
+{
+    [TestFixture]
+    public class BuilderFacets
+    {
+        [Test]
+        public void BuildPerson()
+        {
+            var pb = new PersonBuilder();
+            Person person = pb
+              .Lives
+                .At("123 London Road")
+                .In("London")
+                .WithPostcode("SW12BC")
+              .Works
+                .At("Fabrikam")
+                .AsA("Engineer")
+                .Earning(123000);
+
+            person.City.Should().BeEquivalentTo("London");
+            person.AnnualIncome.ShouldBeEquivalentTo(123000);  
+        }
+    }
+
+    public class Person
+    {
+        // address
+        public string StreetAddress, Postcode, City;
+
+        // employment
+        public string CompanyName, Position;
+
+        public int AnnualIncome;
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetAddress)}: {StreetAddress}, {nameof(Postcode)}: {Postcode}, {nameof(City)}: {City}, {nameof(CompanyName)}: {CompanyName}, {nameof(Position)}: {Position}, {nameof(AnnualIncome)}: {AnnualIncome}";
+        }
+    }
+
+    public class PersonBuilder // facade 
+    {
+        // the object we're going to build
+        protected Person person = new Person(); // this is a reference!
+
+        public PersonAddressBuilder Lives => new PersonAddressBuilder(person);
+        public PersonJobBuilder Works => new PersonJobBuilder(person);
+
+        public static implicit operator Person(PersonBuilder pb)
+        {
+            return pb.person;
+        }
+    }
+
+    /// <summary>
+    /// PersonJobBuilder  must inherit from PersonBuilder, otherwise you can't do the following:
+    /// 
+    /// var pb = new PersonBuilder();
+    ///       Person person = pb
+    ///                       .Lives
+    ///                       .At("123 London Road")
+    ///                       .Works                   <-- The call to Works won't compile!!!
+    ///                       .At("someCompany");
+    /// 
+    /// </summary>
+    public class PersonJobBuilder : PersonBuilder
+    {
+        public PersonJobBuilder(Person person)
+        {
+            this.person = person;
+        }
+
+        public PersonJobBuilder At(string companyName)
+        {
+            person.CompanyName = companyName;
+            return this;
+        }
+
+        public PersonJobBuilder AsA(string position)
+        {
+            person.Position = position;
+            return this;
+        }
+
+        public PersonJobBuilder Earning(int annualIncome)
+        {
+            person.AnnualIncome = annualIncome;
+            return this;
+        }
+    }
+
+    public class PersonAddressBuilder : PersonBuilder
+    {
+
+        // might not work with a value type!
+        public PersonAddressBuilder(Person person)
+        {
+            this.person = person;
+        }
+
+        public PersonAddressBuilder At(string streetAddress)
+        {
+            person.StreetAddress = streetAddress;
+            return this;
+        }
+
+        public PersonAddressBuilder WithPostcode(string postcode)
+        {
+            person.Postcode = postcode;
+            return this;
+        }
+
+        public PersonAddressBuilder In(string city)
+        {
+            person.City = city;
+            return this;
+        }
+    }
+}
+
+
